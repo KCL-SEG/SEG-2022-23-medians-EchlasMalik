c74900ccff7937522d8d04e0a2178f60bf9ae02c
Echlas Malik <EchlasMalik1@gmail.com>
2022-10-05 16:47:34 +0100

    New Changes Made

--- median.py
+++ median.py
@@ -1,12 +1,15 @@
-"""Median calculator."""
-"""ENTER YOUR SOLUTION HERE!"""
-
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+        evenMedian = 0
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+    if len(numbers) % 2 == 0:
+        evenMedian = numbers[len(numbers)] + numbers[len(numbers)+1] / 2
+        print(evenMedian)
+    elif len(numbers) % 2 != 0:
+        evenMedian = -(numbers[len(numbers)] / 2);
+print(evenMedian)
